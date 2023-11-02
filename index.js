0cb15b67c7ab7b5f047f17fbe61cf09d0310fdcf
Marius Alban <malban298@gmail.com>
2023-11-02 11:34:11 -0300

    Jscript linked

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-
-let name;
-let nameForm = document.getElementById('name-form');
-document.addEventListener('DOMContentLoaded', () => {
-nameForm.addEventListener('submit', e => {e.preventDefault();
-
-})})
-
-function fetchName(){
-    fetch(`https://api.nationalize.io/?name=${name}`)
-    .then(resp => resp.json())
-    .then(nameInfo => nameInfo.forEach())
-};
+document.addEventListener("DOMContentLoaded", () => console.log('connected'))

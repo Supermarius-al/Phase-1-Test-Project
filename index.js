676176b9db2c0dc56c78960db2c241ec80fabd78
Marius Alban <malban298@gmail.com>
2023-11-04 13:47:46 -0300

    added table and linked json

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const form = document.getElementById("workout_select")
 submitBtn.addEventListener('click', (e) =>{
 e.preventDefault()
 let workoutSelect = document.querySelector('input[type="radio"]:checked').value
+})
+})
 
-})
-})
+function renderMaintain(){
+    fetch("http://localhost:3000/workoutTypes")
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+}
